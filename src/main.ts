7c0d96bf2228d74609dd70e278fd5be4d6096495
Kacper Stojczyk <kacper.stojczyk@student.pxl.be>
2023-12-09 14:06:04 +0100

    implemented datadog

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,25 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { datadogRum } from '@datadog/browser-rum';
+
+const envi = process.env.ENVI || 'dev'; 
+const appID = process.env.APP_ID || 'appid';
+const client = process.env.CLIENT || 'client';
+
+datadogRum.init({
+    applicationId: appID,
+    clientToken: client,
+    site: 'us5.datadoghq.com',
+    service: 'frontend',
+    env: envi,
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 20,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel: 'mask-user-input',
+});
 
 if (environment.production) {
   enableProdMode();
